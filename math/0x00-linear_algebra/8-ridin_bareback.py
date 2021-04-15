ca4b091b0233aa1aa341136e44efdcd9447a3a2d
Fadi Hanna <75306106+fadygithub@users.noreply.github.com>
2021-04-15 13:11:23 +0300

    Fixed pep8 format for 8-ridin_bareback.py

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -7,6 +7,5 @@ def mat_mul(mat1, mat2):
     if len(mat1[0]) != len(mat2):
         return None
     new_matrix = [[sum(a * b for a, b in zip(mat1_rows, mat2_column))
-                for mat2_column in zip(*mat2)]
-               for mat1_rows in mat1]
+                  for mat2_column in zip(*mat2)]for mat1_rows in mat1]
     return new_matrix
