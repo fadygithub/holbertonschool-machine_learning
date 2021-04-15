d397e8c3123a979b6f3dc0c6351f3a76783def98
Fadi Hanna <75306106+fadygithub@users.noreply.github.com>
2021-04-15 14:01:46 +0300

    Push code for 12-bracin_the_elements.py

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function for np_elementwise"""
+
+
+def np_elementwise(mat1, mat2):
+    """Perform element-wise addition, subtraction, multiplication, and division on a Numpy array"""
+    return(mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
