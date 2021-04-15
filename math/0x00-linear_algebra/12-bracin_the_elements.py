33ce8babf21194dec8841253a9cdd74da545b38a
Fadi Hanna <75306106+fadygithub@users.noreply.github.com>
2021-04-15 14:05:51 +0300

    Fixed a pep8 format for 12-bracin_the_elements.py

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -3,5 +3,5 @@
 
 
 def np_elementwise(mat1, mat2):
-    """Perform element-wise addition, subtraction, multiplication, and division on a Numpy array"""
+    """Perform element-wise operations on a Numpy array"""
     return(mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
