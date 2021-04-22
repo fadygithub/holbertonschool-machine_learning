23e1bbc14b8179179a72e73d28732a3c6bca8f0a
Fadi Hanna <75306106+fadygithub@users.noreply.github.com>
2021-04-22 21:05:59 +0300

    debug the file 9-sum_total.py

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+"""Calculates sum of a squared variable"""
 
-summation_i_squared = __import__('9-sum_total').summation_i_squared
 
-n = 5
-print(summation_i_squared(n))
+def summation_i_squared(n):
+    """Summation i squared"""
+    if type(n) != int or n < 1:
+        return None
+    return (n * (n + 1) * (2 * n + 1)) // 6
 
