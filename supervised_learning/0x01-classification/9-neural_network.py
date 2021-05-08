59f4d7a3e8b1922c2c1ecf1f608dddd5616991e4
Fady Hanna <fadyhanna1@outlook.com>
2021-05-08 15:10:52 +0300

    Add the file 9-neural_network

---
+++ supervised_learning/0x01-classification/9-neural_network.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Module for neural network"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """Clas NeuralNetwork"""
+    def __init__(self, nx, nodes):
+        """Data Initialization"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) != int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
+
+    @property
+    def W1(self):
+        """Getter for W1"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for b1"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for A1"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for W2"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for b2"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for A2"""
+        return self.__A2
