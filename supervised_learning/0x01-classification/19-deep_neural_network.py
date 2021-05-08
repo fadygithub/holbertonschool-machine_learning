f534c796f1465884d92586967ece94b0698e1a2f
Fady Hanna <fadyhanna1@outlook.com>
2021-05-08 18:22:04 +0300

    Add the file 19-deep_neural_network.py

---
+++ supervised_learning/0x01-classification/19-deep_neural_network.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Deep neural network module"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Class DeepNeuralNetwork"""
+    def __init__(self, nx, layers):
+        """Data initialization"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) != list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        layer = 1
+        layer_size = nx
+        for i in layers:
+            if type(i) != int or i <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            w = "W" + str(layer)
+            b = "b" + str(layer)
+            self.__weights[w] = np.random.randn(
+                i, layer_size) * np.sqrt(2/layer_size)
+            self.__weights[b] = np.zeros((i, 1))
+            layer += 1
+            layer_size = i
+
+    @property
+    def L(self):
+        """Getter function for L"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """Getter function for cache"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """Getter function for weights"""
+        return self.__weights
+
+    def forward_prop(self, X):
+        """Forward propagation calculation"""
+        self.__cache["A0"] = X
+        for i in range(1, self.__L + 1):
+            w = "W" + str(i)
+            b = "b" + str(i)
+            a = "A" + str(i - 1)
+            Z = np.matmul(self.__weights[w],
+                          self.__cache[a]) + self.__weights[b]
+            a_new = "A" + str(i)
+            self.__cache[a_new] = 1 / (1 + np.exp(-Z))
+        Act = "A" + str(self.__L)
+        return (self.__cache[Act], self.__cache)
+
+    def cost(self, Y, A):
+        """Model Cost Calculation"""
+        cost_array = np.multiply(np.log(A), Y) + np.multiply((
+            1 - Y), np.log(1.0000001 - A))
+        cost = -np.sum(cost_array) / len(A[0])
+        return cost
