553be33240a77a733c66539595792b25a0e5efe2
Fady Hanna <fadyhanna1@outlook.com>
2021-05-13 21:22:52 +0300

    Add task 0-create_placeholders.py

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Placeholder Module"""
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    """Create a placeholder function"""
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
+    return (x, y)
+
