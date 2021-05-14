843277da8c3b3aa80555b0f0bec90c595b1f8d3a
Fady Hanna <fadyhanna1@outlook.com>
2021-05-14 19:34:17 +0300

    Add the task 7-evaluate.py

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Neural Network Output Evaluation Module"""
+
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """Function to evaluate the output of the neural network"""
+    new_path = save_path + ".meta"
+    saver = tf.train.import_meta_graph(new_path)
+    with tf.Session() as sess:
+        saver.restore(sess, save_path)
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")
+        loss = tf.get_collection("loss")
+        accuracy = tf.get_collection("accuracy")
+        pred, a_val, l_val = sess.run([y_pred, accuracy, loss], {x: X, y: Y})
+        return (pred[0], a_val[0], l_val[0])
