aba690ec3a5a341208a161769ae3f038708c8a7d
Fady Hanna <fadyhanna1@outlook.com>
2021-05-20 00:37:36 +0300

    fix a formating error

--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -5,8 +5,7 @@ import numpy as np
 
 
 def normalization_constants(X):
-    """Function that will return the mean and standard deviation of the inputs"""
+    """Function that returns the mean &  standard deviation"""
     mean = np.mean(X, axis=0)
     stdev = np.std(X, axis=0)
     return mean, stdev
-
