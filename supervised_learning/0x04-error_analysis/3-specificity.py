988c88dafe8a3c4acd20baecfeaf0d24ed5616ae
Fady Hanna <fadyhanna1@outlook.com>
2021-05-24 15:04:25 +0300

    Add task3 for specificity

---
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""Specificity Calculation Module"""
+
+import numpy as np
+
+
+def specificity(confusion):
+    """Function that returns the specificity"""
+    id_ma = np.identity(confusion.shape[0])
+    zero_diagonal = np.where(confusion * id_ma == 0, confusion, 0)
+    false_positives = np.sum(zero_diagonal, axis=0)
+    true_negatives = []
+    for i in range(confusion.shape[0]):
+        if i == 0:
+            true_negatives.append(np.sum(confusion[1:, 1:]))
+        elif i == confusion.shape[0] - 1:
+            true_negatives.append(np.sum(confusion[0:i, 0:i]))
+        else:
+            true_negatives.append(np.sum(confusion[0:i, 0:i]) + np.sum(
+                confusion[0:i, i + 1:]) + np.sum(
+                confusion[i + 1:, i + 1:]) + np.sum(
+                confusion[i + 1:, 0:i]))
+    return true_negatives / (true_negatives + false_positives)
