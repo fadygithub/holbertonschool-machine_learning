af62e3aae69d21889ac29eadff2b551692655a5e
Fady Hanna <fadyhanna1@outlook.com>
2021-05-27 09:38:09 +0300

    Fix a formatting error

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -3,16 +3,6 @@
 import numpy as np
 
 
-# Y is a one-hot numpy.ndarray of shape (classes, m) that contains the correct labels for the data
-# classes is the number of classes
-# m is the number of data points
-# weights is a dictionary of the weights and biases of the neural network
-# cache is a dictionary of the outputs and dropout masks of each layer of the neural network
-# alpha is the learning rate
-# keep_prob is the probability that a node will be kept
-# L is the number of layers of the network
-
-
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """weights of the network should be
        updated in place"""
