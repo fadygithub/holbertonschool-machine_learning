c8eca54d2ff200b99bb31ecfb441a78cecaa9941
Fady Hanna <fadyhanna1@outlook.com>
2021-06-01 14:54:12 +0300

    Add task 6-train.py

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Train with early stopping Module"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """Function that stops the training only if validation_data exist"""
+    if validation_data:
+        early_stop = K.callbacks.EarlyStopping(
+            monitor='val_loss',  mode='min', patience=patience)
+        early_stop = [early_stop]
+    else:
+        early_stop = None
+    History = network.fit(
+                        x=data,
+                        y=labels,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        verbose=verbose,
+                        shuffle=shuffle,
+                        validation_data=validation_data,
+                        callbacks=early_stop)
+    return History
