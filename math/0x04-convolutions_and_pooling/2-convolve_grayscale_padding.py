752f77f6c3a57ed2e4c552062d8aaec2d1061ca4
Fady Hanna <fadyhanna1@outlook.com>
2021-06-03 11:14:04 +0300

    Add task 2-convolve_grayscale_padding.py

---
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Convolution on grayscale images Module"""
+
+import numpy as np
+
+
+def convolve_grayscale_padding(images, kernel, padding):
+    """Function that returns a numpy arraywith the convolved images"""
+    kh, kw = kernel.shape
+    (ph, pw) = padding
+    padded_images = np.pad(
+        images,
+        [(0, 0), (ph, ph), (pw, pw)]
+    )
+    m, h, w = padded_images.shape
+    conv_shape = (m, h - kh + 1, w - kw + 1)
+    conv = np.zeros(conv_shape)
+    for row in range(conv_shape[1]):
+        for column in range(conv_shape[2]):
+            sub_matrix = padded_images[:, row: row + kh, column: column + kw]
+            conv[:, row, column] = (sub_matrix * kernel).sum(axis=(1, 2))
+    return conv
