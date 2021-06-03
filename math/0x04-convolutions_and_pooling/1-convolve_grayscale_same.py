9675130dd6dd52170040d800ce81f5d8e909b18f
Fady Hanna <fadyhanna1@outlook.com>
2021-06-03 10:58:46 +0300

    Add task 1-convolve_grayscale_same.py

---
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Convolution on grayscale images Module"""
+
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """Function that returns a numpy array with the convolved images"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    pw = int(kw / 2 if kw % 2 == 0 else (kw - 1) / 2)
+    ph = int(kh / 2 if kh % 2 == 0 else (kh - 1) / 2)
+    padded_images = np.pad(
+        images,
+        [(0, 0), (ph, ph), (pw, pw)]
+    )
+    conv = np.zeros(images.shape)
+    for row in range(h):
+        for column in range(w):
+            sub_matrix = padded_images[:, row: row + kh, column: column + kw]
+            conv[:, row, column] = (sub_matrix * kernel).sum(axis=(1, 2))
+    return conv
