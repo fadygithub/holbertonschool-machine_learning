ed3b13f598e502736d349445b7c1b884ac34c957
Fady Hanna <fadyhanna1@outlook.com>
2021-06-16 22:00:47 +0300

    Add task number 2

---
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Identity block Module - 2015"""
+import tensorflow.keras as K
+
+
+def identity_block(A_prev, filters):
+    """Function that returns an activated output"""
+    F11, F3, F12 = filters
+    conv1 = K.layers.Conv2D(filters=F11,
+                            kernel_size=(1, 1),
+                            padding="same",
+                            kernel_initializer="he_normal",
+                            strides=(1, 1))(A_prev)
+    BN1 = K.layers.BatchNormalization(axis=3)(conv1)
+    Relu1 = K.layers.Activation("relu")(BN1)
+    conv2 = K.layers.Conv2D(filters=F3,
+                            kernel_size=(3, 3),
+                            padding="same",
+                            kernel_initializer="he_normal",
+                            strides=(1, 1))(Relu1)
+    BN2 = K.layers.BatchNormalization(axis=3)(conv2)
+    Relu2 = K.layers.Activation("relu")(BN2)
+    conv3 = K.layers.Conv2D(filters=F12,
+                            kernel_size=(1, 1),
+                            padding="same",
+                            kernel_initializer="he_normal",
+                            strides=(1, 1))(Relu2)
+    BN3 = K.layers.BatchNormalization(axis=3)(conv3)
+    add_layers = K.layers.Add()([BN3, A_prev])
+    return K.layers.Activation("relu")(add_layers)
